e126c62ac1b525ea0c28a652e86f40c222c93cd0
Fillipedem <fillipedem@gmail.com>
2017-06-04 11:23:55 -0300

    Adicionado arquivo  para ler o banco de dados e separando os valores a serem treinados e aproximados

--- svr.py
+++ svr.py
@@ -2,20 +2,23 @@
 Utiliza implementação SVR do sklearn para aproximar o Path Loss(Attenuation)
 """
 # SVR, numpy
-import sklearn.svm
+from sklearn.svm import SVR
 import numpy as np
 # database
 from database import erbs_header, data_erbs, medicoes_header, data_medicoes
 
 
 # data medicoes
-print(medicoes_header)
 pos = medicoes_header.index('PLreal')
 
+# training data X
+X_header = medicoes_header[:pos] + medicoes_header[pos + 1:]
 X = np.delete(data_medicoes, pos, 1)
-Y = data_medicoes[:, pos]
 
-print("X:")
-print (X)
-print("\n\n\nY:")
-print(Y)
+# target data y
+Y_header = ['PLreal']
+y = data_medicoes[:, pos]
+
+# simple SVR
+clf = SVR(C=1.0, epsilon=0.1)
+clf.fit(X, y)
